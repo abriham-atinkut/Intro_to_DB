81c9c89d64dfcfbf9e097f9899c798aefb3f8583
unknown <abrihamatinkut@gmail.com>
2025-12-11 15:36:34 +0300

    last update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store
 
 
 CREATE TABLE Books (
-    book_id INT (PRIMARY KEY)
+    book_id INT PRIMARY KEY
     title VARCHAR(130)
     FOREIGN KEY (author_id) REFERENCES Authrors (author_id)
     price DOUBLE
@@ -10,25 +10,25 @@ CREATE TABLE Books (
 );
 
 CREATE TABLE Authors (
-    author_id (FOREIGN KEY)
+    author_id PRIMARY KEY
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers (
-    customer_id INT (FOREIGN KEY)
+    customer_id INT PRIMARY KEY
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
 );
 
 CREATE TABLE Orders (
-    order_id INT
-    order_date DATE
+    order_id INT PRIMARY KEY
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    order_date DATE
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid (Primary Key)
+    orderdetailid PRIMARY KEY
     quantity DOUBLE
     FOREIGN KEY (order_id) REFERENCES Orders(order_id)
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
